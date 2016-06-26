67349b0053da8b91a40ca764c78b02bad2570cb9
codevarz <kvkreddy1990@gmail.com>
2016-06-26 11:10:53 -0400

    Update flikrScript.js

--- flikrScript.js
+++ flikrScript.js
@@ -7,15 +7,23 @@ jQuery(document).ready(function () {
 
     var htm;
     var link;
+    var tags = [];
+    var tag = "+";
     function generate_url() {
-        var tags = jQuery('#input').val();
+        var tags = jQuery('#input').val().split(" ");
+        for(size=0;size<tags.length;size++){
+            tag = tag + tags[size]+"+";
+        }
+
+        console.log(tags.toString());
+        console.log(tag);
         link = "https://api.flickr.com/services/rest/?method=flickr.photos.search";
         var key = "b349d94ae45a2fec62d5f95b01bded94";
         var extra = "url_m";
         var format = "json";
 
-        link = link+'&api_key='+key+'&tags='+tags+'&extras='+extra+'&format='+format+"&nojsoncallback=1";
-        console.log(link);
+        link = link+'&api_key='+key+'&tags='+tag+'&extras='+extra+'&format='+format+"&nojsoncallback=1";
+       // console.log(link);
     }
 
 
@@ -34,19 +42,21 @@ jQuery(document).ready(function () {
                 var programmes = data.photos.photo;
                 for(i=0;i<6;i++){
                     image_src.push(data.photos.photo[i].url_m);
-                    console.log(image_src[i])
+                   // console.log(image_src[i])
                 }
-                var temp = "slide"+1;
-                console.log(temp);
+               // var temp = "slide"+1;
+               // console.log(temp);
                 //$('.'"slide"+1'').attr('src', image_src[1]) ;
-                console.log(image_src.toString());
+                //console.log(image_src.toString());
 
                 $('.slide').each(function (index, value) {
-                    console.log(index);
+                    //console.log(index);
                     $(this).attr('src',image_src[index])
                 })
 
                 $('#slide_cont').show();
+                tag = "+"
+
 
             },
             error: function (exception) {
